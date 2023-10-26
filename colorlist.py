d36967911708273c45d0cec4a8ebb944e756a496
Ansar-Thangalparambil <145666962+Ansar-Thangalparambil@users.noreply.github.com>
2023-10-26 16:43:21 +0530

    Delete colorlist.py

--- colorlist.py
+++
@@ -1,4 +0,0 @@
-#Print out all the colors from the colorr_list1 not contained in color-list2
-color_list1=set(["White","Black","Red"])
-color_list2=set(["Red","Green"])
-print(color_list1-color_list2)
