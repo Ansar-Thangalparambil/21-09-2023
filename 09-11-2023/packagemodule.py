f0a734847dcb6e6b81a6762ebceef99e71c5b35b
Ansar <145666962+Ansar-Thangalparambil@users.noreply.github.com>
2023-11-09 16:29:43 +0530

    Add files via upload

---
+++ 09-11-2023/packagemodule.py
@@ -0,0 +1,10 @@
+from Package import summodule as sum1
+from Package import avgmodule as avg1
+from Package import power as pow1
+
+a=sum1.sum(10,20)
+b=avg1.average(10,20)
+c=pow1.power(2,3)
+print(a)
+print(b)
+print(c)
